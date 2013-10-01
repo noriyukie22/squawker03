ceefdc8ae925cb40064541d13566d89dda8ca45e
tadahiro <tadahiro@example.com>
2013-10-01 22:33:24 +0900

    Add to Local Repository

---
+++ src/net/jazz/uws/squawkers/Penguin.java
@@ -0,0 +1,14 @@
+package net.jazz.uws.squawkers;
+
+import net.jazz.uws.squawk.Squawker;
+
+public class Penguin extends Squawker {
+
+	@Override
+	public String squawk() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
